61e9093074d99b9718781028e6f3d23947578bba
Vaisakh N <vaisakhnofficial@gmail.com>
2026-04-02 22:33:38 +0530

    Refactor app.js for improved structure and clarity

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+
+app.set('trust proxy', 1);
+
 const path =require('path');
 const userRouter = require('./routes/userRouter');
 const adminRouter = require('./routes/adminRouter') 
