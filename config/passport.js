e9518c181a6c6c97a7def2c822e465d5dd1f454d
Vaisakh N <vaisakhnofficial@gmail.com>
2026-04-02 22:32:28 +0530

    Modify Google OAuth callback URL configuration
    Updated callback URL to use BASE_URL environment variable.

--- config/passport.js
+++ config/passport.js
@@ -16,7 +16,7 @@ function generateReferralCode() {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: process.env.BASE_URL + "/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
